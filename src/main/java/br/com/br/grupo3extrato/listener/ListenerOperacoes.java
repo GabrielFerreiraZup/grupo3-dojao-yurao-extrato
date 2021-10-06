c054bc963ecf06fb02db633d1581dec3fbd9039f
CarolinaFaria <carolina.faria@zup.com.br>
2021-10-06 18:02:55 -0300

    salva no banco ok

--- src/main/java/br/com/br/grupo3extrato/listener/ListenerOperacoes.java
+++ src/main/java/br/com/br/grupo3extrato/listener/ListenerOperacoes.java
@@ -2,16 +2,21 @@ package br.com.br.grupo3extrato.listener;
 
 
 import br.com.br.grupo3extrato.model.EventoExtrato;
+import br.com.br.grupo3extrato.repository.EventoExtratoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ListenerOperacoes {
 
+    @Autowired
+    EventoExtratoRepository repository;
 
-    @KafkaListener(topics = "operacao")
+    @KafkaListener(topics = "conta_transacao")
     public void ouvir (EventoExtrato eventoExtrato){
         System.out.println(eventoExtrato.toString());
+        repository.save(eventoExtrato);
     }
 
 }
